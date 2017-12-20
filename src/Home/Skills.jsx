a30b6d97da9c21e2d25d9ad1b2299c47039c5e6c
dayfine <difancpa@gmail.com>
2017-12-20 18:19:19 -0500

    experimenting with metal texture

--- src/Home/Skills.jsx
+++ src/Home/Skills.jsx
@@ -5,19 +5,22 @@ import Chip from 'material-ui/Chip'
 import Typography from 'material-ui/Typography'
 
 const skills = [
-  'React', 'JavaScript', 'ES6/Babel', 'Node.js',
-  'SQL', 'PostgreSQL'
+  'React', 'JavaScript', 'ES6/Babel', 'Redux',
+  'Node.js', 'Express', 'SQL', 'PostgreSQL',
+  'HTML5/CSS3', 'Sass', 'Material Design', 'Bootstrap',
+  'Git/GitHub', 'TDD', 'Heroku', 'AWS',
+  'Python', 'PHP'
 ]
 
 function Skills ({ classes }) {
   return (
     <CenterPaper>
-      <div className='row center'>
+      <div className='row start-xs'>
         {skills.map(skill => (
           <Chip
             label={skill}
             key={skill}
-            className='col-xs-4 col-sm-3 col-md-2'
+            className='col-xs-4 col-sm-3 col-md-2 col-xl-1'
             classes={{
               root: classes.chip
             }}
@@ -31,8 +34,15 @@ function Skills ({ classes }) {
 const styles = {
   chip: {
     margin: '8px 12px',
-    background: '#e9e9e9',
-    borderRadius: '8px'
+    // background: '#e9e9e9',
+    borderRadius: '8px',
+    backgroundImage: `-webkit-repeating-linear-gradient(left, hsla(0,0%,100%,0) 0%, hsla(0,0%,100%,0) 6%, hsla(0,0%,100%, .1) 7.5%),
+    -webkit-repeating-linear-gradient(left, hsla(0,0%, 0%,0) 0%, hsla(0,0%,  0%,0) 4%, hsla(0,0%,  0%,.03) 4.5%),
+    -webkit-repeating-linear-gradient(left, hsla(0,0%,100%,0) 0%, hsla(0,0%,100%,0) 1.2%, hsla(0,0%,100%,.15) 2.2%),
+    linear-gradient(180deg, hsl(0,0%,78%)  0%,
+    hsl(0,0%,90%) 47%,
+    hsl(0,0%,78%) 53%,
+    hsl(0,0%,70%)100%)`
   }
 }
 
